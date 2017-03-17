01080047450f5abb2f9753bf58c5b5517db8173d
Davin McCall <davmac@davmac.org>
2017-03-17 17:29:07 +0000

    Add some function tests

--- tests/dasynq-tests.cc
+++ tests/dasynq-tests.cc
@@ -90,6 +90,111 @@ void testFdWatch2()
     watcher2->deregister(my_loop);
 }
 
+static void create_pipe(int filedes[2])
+{
+    if (pipe(filedes) == -1) {
+        throw std::system_error(errno, std::system_category());
+    }
+}
+
+void ftestFdWatch1()
+{
+    using Loop_t = dasynq::event_loop<dasynq::NullMutex>;
+    Loop_t my_loop;
+
+    bool seen1 = false;
+    bool seen2 = false;
+
+    int pipe1[2];
+    int pipe2[2];
+    create_pipe(pipe1);
+    create_pipe(pipe2);
+
+    Loop_t::FdWatcher::addWatch(my_loop, pipe1[0], dasynq::IN_EVENTS,
+            [&seen1](Loop_t &eloop, int fd, int flags) -> rearm {
+        seen1 = true;
+        return rearm::REMOVE;
+    });
+
+    Loop_t::FdWatcher::addWatch(my_loop, pipe2[0], dasynq::IN_EVENTS,
+            [&seen2](Loop_t &eloop, int fd, int flags) -> rearm {
+        seen2 = true;
+        return rearm::REMOVE;
+    });
+
+    // test_io_engine::trigger_fd_event(0, dasynq::IN_EVENTS);
+    char wbuf[1] = {'a'};
+    char rbuf[1];
+    write(pipe1[1], wbuf, 1);
+
+    my_loop.run();
+
+    assert(seen1);
+    assert(!seen2);
+
+    seen1 = false;
+
+    read(pipe1[0], rbuf, 1);
+
+    //test_io_engine::trigger_fd_event(0, dasynq::IN_EVENTS);
+    //test_io_engine::trigger_fd_event(1, dasynq::IN_EVENTS);
+    write(pipe1[1], wbuf, 1);
+    write(pipe2[1], wbuf, 1);
+
+    my_loop.run();
+
+    assert(!seen1);
+    assert(seen2);
+
+    close(pipe1[0]);
+    close(pipe1[1]);
+    close(pipe2[0]);
+    close(pipe2[1]);
+}
+
+void ftestSigWatch()
+{
+    using Loop_t = dasynq::event_loop<dasynq::NullMutex>;
+    Loop_t my_loop;
+
+    bool seen1 = false;
+    bool seen2 = false;
+
+    using SigInfo_p = Loop_t::SignalWatcher::SigInfo_p;
+
+    Loop_t::SignalWatcher::addWatch(my_loop, SIGUSR1,
+            [&seen1](Loop_t &eloop, int signo, SigInfo_p info) -> rearm {
+        seen1 = true;
+        return rearm::REMOVE;
+    });
+
+    Loop_t::SignalWatcher::addWatch(my_loop, SIGUSR2,
+            [&seen2](Loop_t &eloop, int signo, SigInfo_p info) -> rearm {
+        seen2 = true;
+        return rearm::REMOVE;
+    });
+
+    // test_io_engine::trigger_fd_event(0, dasynq::IN_EVENTS);
+    raise(SIGUSR1);
+
+    my_loop.run();
+
+    assert(seen1);
+    assert(!seen2);
+
+    seen1 = false;
+
+    //test_io_engine::trigger_fd_event(0, dasynq::IN_EVENTS);
+    //test_io_engine::trigger_fd_event(1, dasynq::IN_EVENTS);
+    raise(SIGUSR1);
+    raise(SIGUSR2);
+
+    my_loop.run();
+
+    assert(!seen1);
+    assert(seen2);
+}
+
 int main(int argc, char **argv)
 {
     std::cout << "testFdWatch1... ";
@@ -98,6 +203,14 @@ int main(int argc, char **argv)
 
     std::cout << "testFdWatch2... ";
     testFdWatch2();
+    std::cout << "PASSED" << std::endl;
+
+    std::cout << "ftestFdWatch1... ";
+    ftestFdWatch1();
+    std::cout << "PASSED" << std::endl;
+
+    std::cout << "ftestSigWatch... ";
+    ftestSigWatch();
     std::cout << "PASSED" << std::endl;
     
     return 0;
