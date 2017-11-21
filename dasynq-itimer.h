a618d04e0a3051e21d5da7015585dd11361fa1b2
Davin McCall <davmac@davmac.org>
2017-11-21 04:09:42 +0000

    itimer: support seperate monotonic clock.
    Original implementation supported only a single clock and timer queue.
    Now, if CLOCK_MONOTONIC is defined, a separate timer queue is maintained
    and used for monotonic timers. However, as before, timers against the
    SYSTEM clock (CLOCK_REALTIME) may expire at an incorrect time if the
    system clock time is altered.

--- dasynq-itimer.h
+++ dasynq-itimer.h
@@ -18,18 +18,33 @@ template <class Base> class itimer_events : public timer_base<Base>
     private:
     timer_queue_t timer_queue;
 
+
 #if defined(CLOCK_MONOTONIC)
-    static inline void get_curtime(struct timespec &curtime)
+    timer_queue_t mono_timer_queue;
+
+    inline timer_queue_t &queue_for_clock(clock_type clock)
     {
-        clock_gettime(CLOCK_MONOTONIC, &curtime);
+        if (clock == clock_type::MONOTONIC) {
+            return mono_timer_queue;
+        }
+        else {
+            return timer_queue;
+        }
+    }
+
+    inline bool timer_queues_empty()
+    {
+        return timer_queue.empty() && mono_timer_queue.empty();
     }
 #else
-    static inline void get_curtime(struct timespec &curtime)
+    inline timer_queue_t &queue_for_clock(clock_type clock)
     {
-        struct timeval curtime_tv;
-        gettimeofday(&curtime_tv, nullptr);
-        curtime.tv_sec = curtime_tv.tv_sec;
-        curtime.tv_nsec = curtime_tv.tv_usec * 1000;
+        return timer_queue;
+    }
+
+    inline bool timer_queues_empty()
+    {
+        return timer_queue.empty();
     }
 #endif
     
@@ -39,7 +54,7 @@ template <class Base> class itimer_events : public timer_base<Base>
     {
         time_val newtime;
         struct itimerval newalarm;
-        if (timer_queue.empty()) {
+        if (timer_queues_empty()) {
             newalarm.it_value = {0, 0};
             newalarm.it_interval = {0, 0};
             setitimer(ITIMER_REAL, &newalarm, nullptr);
@@ -47,24 +62,38 @@ template <class Base> class itimer_events : public timer_base<Base>
         }
 
         newtime = timer_queue.get_root_priority();
-        
-        struct timespec curtime;
-        get_curtime(curtime);
-        time_val curtimev = curtime;
 
-        newalarm.it_interval = {0, 0};
+        time_val curtimev;
+        timer_base<Base>::get_time(curtimev, clock_type::SYSTEM, true);
+
+        // interval before next timeout:
+        time_val interval_tv = {0, 0};
         if (curtimev < newtime) {
-            newalarm.it_value.tv_sec = newtime.seconds() - curtime.tv_sec;
-            if (curtimev.nseconds() > newtime.nseconds()) {
-                newalarm.it_value.tv_usec = (1000000000 - curtimev.nseconds()
-                        + newtime.nseconds()) / 1000;
-                newalarm.it_value.tv_sec--;
-            }
-            else {
-                newalarm.it_value.tv_usec = (newtime.nseconds() - curtime.tv_nsec) / 1000;
-            }
+            interval_tv = newtime - curtimev;
         }
-        else {
+
+#ifdef CLOCK_MONOTONIC
+        // If we have a separate monotonic clock, we get the interval for the expiry of the next monotonic
+        // timer and use the lesser of the system interval and monotonic interval:
+        time_val mono_newtime = mono_timer_queue.get_root_priority();
+
+        time_val curtimev_mono;
+        timer_base<Base>::get_time(curtimev_mono, clock_type::MONOTONIC, true);
+
+        time_val interval_mono = {0, 0};
+        if (curtimev_mono < mono_newtime) {
+            interval_mono = mono_newtime - curtimev_mono;
+        }
+
+        if (interval_mono < interval_tv) {
+            interval_tv = interval_mono;
+        }
+#endif
+
+        newalarm.it_interval.tv_sec = interval_tv.seconds();
+        newalarm.it_interval.tv_usec = interval_tv.nseconds() / 1000;
+
+        if (newalarm.it_interval.tv_sec == 0 && newalarm.it_interval.tv_usec == 0) {
             // We passed the timeout: set alarm to expire immediately (we must use {0,1} as
             // {0,0} disables the timer).
             // TODO: it would be better if we just processed the appropriate timers here,
@@ -86,11 +115,16 @@ template <class Base> class itimer_events : public timer_base<Base>
     {
         if (siginfo.get_signo() == SIGALRM) {
             struct timespec curtime;
-            get_curtime(curtime);
-            
+            timer_base<Base>::get_time(curtime, clock_type::SYSTEM, true);
             timer_base<Base>::process_timer_queue(timer_queue, curtime);
+            
+#ifdef CLOCK_MONOTONIC
+            struct timespec curtime_mono;
+            timer_base<Base>::get_time(curtime_mono, clock_type::MONOTONIC, true);
+            timer_base<Base>::process_timer_queue(mono_timer_queue, curtime_mono);
+#endif
 
-            // arm timerfd with timeout from head of queue
+            // arm alarm with timeout from head of queue
             set_timer_from_queue();
             return false; // don't disable signal watch
         }
@@ -119,7 +153,7 @@ template <class Base> class itimer_events : public timer_base<Base>
     void add_timer(timer_handle_t &h, void *userdata, clock_type clock = clock_type::MONOTONIC)
     {
         std::lock_guard<decltype(Base::lock)> guard(Base::lock);
-        timer_queue.allocate(h, userdata);
+        queue_for_clock(clock).allocate(h, userdata);
     }
     
     void remove_timer(timer_handle_t &timer_id, clock_type clock = clock_type::MONOTONIC) noexcept
@@ -130,6 +164,7 @@ template <class Base> class itimer_events : public timer_base<Base>
     
     void remove_timer_nolock(timer_handle_t &timer_id, clock_type clock = clock_type::MONOTONIC) noexcept
     {
+        auto &timer_queue = queue_for_clock(clock);
         if (timer_queue.is_queued(timer_id)) {
             timer_queue.remove(timer_id);
         }
@@ -141,6 +176,7 @@ template <class Base> class itimer_events : public timer_base<Base>
     void set_timer(timer_handle_t &timer_id, const time_val &timeouttv, const time_val &intervaltv,
             bool enable, clock_type clock = clock_type::MONOTONIC) noexcept
     {
+        auto &timer_queue = queue_for_clock(clock);
         timespec timeout = timeouttv;
         timespec interval = intervaltv;
 
@@ -171,9 +207,8 @@ template <class Base> class itimer_events : public timer_base<Base>
         timespec timeout = timeouttv;
         timespec interval = intervaltv;
 
-        // TODO consider caching current time somehow; need to decide then when to update cached value.
         struct timespec curtime;
-        get_curtime(curtime);
+        timer_base<Base>::get_time(curtime, clock, false);
         curtime.tv_sec += timeout.tv_sec;
         curtime.tv_nsec += timeout.tv_nsec;
         if (curtime.tv_nsec > 1000000000) {
@@ -190,8 +225,9 @@ template <class Base> class itimer_events : public timer_base<Base>
         enableTimer_nolock(timer_id, enable, clock);
     }
     
-    void enableTimer_nolock(timer_handle_t &timer_id, bool enable, clock_type = clock_type::MONOTONIC) noexcept
+    void enableTimer_nolock(timer_handle_t &timer_id, bool enable, clock_type clock = clock_type::MONOTONIC) noexcept
     {
+        auto &timer_queue = queue_for_clock(clock);
         auto &node_data = timer_queue.node_data(timer_id);
         auto expiry_count = node_data.expiry_count;
         if (expiry_count != 0) {
@@ -211,6 +247,7 @@ template <class Base> class itimer_events : public timer_base<Base>
 
     void stop_timer_nolock(timer_handle_t &timer_id, clock_type clock = clock_type::MONOTONIC) noexcept
     {
+        auto &timer_queue = queue_for_clock(clock);
         if (timer_queue.is_queued(timer_id)) {
             bool was_first = (&timer_queue.get_root()) == &timer_id;
             timer_queue.remove(timer_id);
@@ -218,18 +255,6 @@ template <class Base> class itimer_events : public timer_base<Base>
                 set_timer_from_queue();
             }
         }
-    }
-
-    void get_time(time_val &tv, clock_type clock, bool force_update) noexcept
-    {
-        timespec ts;
-        get_time(ts, clock, force_update);
-        tv = ts;
-    }
-
-    void get_time(timespec &ts, clock_type clock, bool force_update) noexcept
-    {
-        get_curtime(ts);
     }
 };
 
