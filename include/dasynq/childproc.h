cad40039c68b338d0f5fec5b6d1487bceee971ea
Davin McCall <davmac@davmac.org>
2024-09-16 22:12:55 +1000

    Fix comment re portability
    Although the OS bug which is apparent in MacOS also manifested on Alpine
    Linux ARM-based build nodes, having looked at Musl source I think this
    was probably an emulation issue.

--- include/dasynq/childproc.h
+++ include/dasynq/childproc.h
@@ -137,7 +137,7 @@ template <class Base> class child_proc_events : public Base
             reaper_lock.lock();
 
             siginfo_t child_info;
-            child_info.si_pid = 0; // for portability inc. Musl, MacOS
+            child_info.si_pid = 0; // for portability inc. MacOS
             while (waitid(P_ALL, 0 /* ignored */, &child_info, WNOHANG | WEXITED) == 0) {
                 pid_t child = child_info.si_pid;
                 if (child == 0) break;
